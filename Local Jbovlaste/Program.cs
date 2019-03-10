8bdb84de9b559acc8715dee66638e1461e1ed65d
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-10 18:49:47 +0900

    Add Program.cs

--- Local Jbovlaste/Program.cs
+++ Local Jbovlaste/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace Local_Jbovlaste
 {
+    public struct User
+    {
+        string UserName { get; set; }
+        string RealName { get; set; }
+    }
+
+    public class Valsi
+    {
+        bool UnOfficial { get; set; }
+        User User { get; set; }
+        string Definition { get; set; }
+        int DefinitionId { get; set; }
+        string Notes { get; set; }
+    }
+
+    public class Gismu : Valsi
+    {
+        List<string> Rafsi { get; set; }
+    }
+
+    public class Cmavo : Valsi
+    {
+        string Selmaho { get; set; }
+    }
+
+    public class JbovlasteDictionary : SortedDictionary<string, Valsi>
+    {
+        public JbovlasteDictionary() { }
+        public JbovlasteDictionary(string filename)
+        {
+            Load(filename);
+        }
+
+        public void Load(string filename)
+        {
+            var doc = new XmlDocument();
+            doc.Load(filename);
+            var dictionary = doc.SelectNodes("dictionary/direction/valsi");
+            foreach (XmlElement node in dictionary)
+            {
+                var type = node.Attributes["type"].InnerText;
+                if (type == "gismu") ;
+                else if (Regex.IsMatch(type, "(cmavo)|(cmavo-compound)|(experimental cmavo)|(bu-letteral)")) ;
+                else if (Regex.IsMatch(type, "(fu'ivla)|(lujvo)")) ;
+                if (node.Attributes["word"].InnerText)
+                        Console.WriteLine(node.Attributes["word"].InnerText);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var doc = new XmlDocument();
+            doc.Load("banjupunu.xml");
+            var dictionary = doc.SelectNodes("dictionary/direction/valsi");
+            foreach (XmlElement node in dictionary)
+            {
+                Console.WriteLine(node.Attributes["word"].InnerText);
+            }
         }
     }
 }
