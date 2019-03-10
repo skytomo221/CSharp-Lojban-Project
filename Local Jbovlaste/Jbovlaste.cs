90f9dd9fba68a0f23447d74c9678fb681a221379
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-10 20:26:07 +0900

    Add Jbovlaste.cs

---
+++ Local Jbovlaste/Jbovlaste.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Jbovlaste
+{
+    public struct User : IEnumerable<string>
+    {
+        public string UserName { get; set; }
+        public string RealName { get; set; }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return UserName;
+            yield return RealName;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return UserName;
+            yield return RealName;
+        }
+
+        public void Add(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Valsi
+    {
+        public bool UnOfficial { get; set; }
+        public string Word { get; set; }
+        public string Type { get; set; }
+        public string Selmaho { get; set; }
+        public List<string> Rafsi { get; set; }
+        public User User { get; set; }
+        public string Definition { get; set; }
+        public int DefinitionId { get; set; }
+        public string Notes { get; set; }
+        public List<string> GlossWord { get; set; }
+    }
+
+    public class Dictionary : List<Valsi>
+    {
+        public Dictionary() { }
+        public Dictionary(string filename)
+        {
+            Load(filename);
+        }
+
+        public void Load(string filename)
+        {
+            var doc = new XmlDocument();
+            doc.Load(filename);
+            var dictionary = doc.SelectNodes("dictionary/direction/valsi");
+            foreach (XmlElement node in dictionary)
+            {
+                var valsi = new Valsi();
+                valsi.UnOfficial = node.GetAttribute("unofficial") == bool.TrueString;
+                valsi.Word = node.GetAttribute("word");
+                valsi.Type = node.GetAttribute("type");
+                valsi.Selmaho = node.GetAttribute("selmaho");
+                var user = (XmlElement)node.SelectSingleNode("user");
+                valsi.User = new User
+                {
+                    UserName = user.SelectSingleNode("username").InnerText,
+                    RealName = user.SelectSingleNode("realname").InnerText,
+                };
+                valsi.Definition = node.SelectSingleNode("definition").InnerText;
+                valsi.DefinitionId = int.Parse(node.SelectSingleNode("definitionid").InnerText);
+                if (node.SelectSingleNode("notes") != null)
+                    valsi.Notes = node.SelectSingleNode("notes").InnerText;
+                valsi.GlossWord = new List<string>();
+                foreach (XmlElement item in node.SelectNodes("glossword"))
+                {
+                    valsi.GlossWord.Add(item.InnerText);
+                }
+                Add(valsi);
+            }
+        }
+
+        public Valsi this[string word]
+        {
+            set
+            {
+                var result = Find(valsi => valsi.Word == word);
+                result = value;
+            }
+            get
+            {
+                return Find(valsi => valsi.Word == word);
+            }
+        }
+    }
+}
--- Local Jbovlaste/Program.cs
+++ Local Jbovlaste/Program.cs
@@ -1,71 +1,26 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Linq;
 using System.Xml;
 
 namespace Local_Jbovlaste
 {
-    public struct User
-    {
-        string UserName { get; set; }
-        string RealName { get; set; }
-    }
-
-    public class Valsi
-    {
-        bool UnOfficial { get; set; }
-        User User { get; set; }
-        string Definition { get; set; }
-        int DefinitionId { get; set; }
-        string Notes { get; set; }
-    }
-
-    public class Gismu : Valsi
-    {
-        List<string> Rafsi { get; set; }
-    }
-
-    public class Cmavo : Valsi
-    {
-        string Selmaho { get; set; }
-    }
-
-    public class JbovlasteDictionary : SortedDictionary<string, Valsi>
-    {
-        public JbovlasteDictionary() { }
-        public JbovlasteDictionary(string filename)
-        {
-            Load(filename);
-        }
-
-        public void Load(string filename)
-        {
-            var doc = new XmlDocument();
-            doc.Load(filename);
-            var dictionary = doc.SelectNodes("dictionary/direction/valsi");
-            foreach (XmlElement node in dictionary)
-            {
-                var type = node.Attributes["type"].InnerText;
-                if (type == "gismu") ;
-                else if (Regex.IsMatch(type, "(cmavo)|(cmavo-compound)|(experimental cmavo)|(bu-letteral)")) ;
-                else if (Regex.IsMatch(type, "(fu'ivla)|(lujvo)")) ;
-                if (node.Attributes["word"].InnerText)
-                        Console.WriteLine(node.Attributes["word"].InnerText);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var doc = new XmlDocument();
-            doc.Load("banjupunu.xml");
-            var dictionary = doc.SelectNodes("dictionary/direction/valsi");
-            foreach (XmlElement node in dictionary)
-            {
-                Console.WriteLine(node.Attributes["word"].InnerText);
-            }
+            var jbovlaste = new Jbovlaste.Dictionary("banjupunu.xml");
+            //var result = from valsi in jbovlaste
+            //             where valsi.Notes == null
+            //             select valsi;
+            //foreach (Valsi valsi in result)
+            //{
+            //    System.Console.WriteLine(valsi.Word);
+            //}
+            System.Console.WriteLine(jbovlaste["cidjrbentou"].Notes);
+            jbovlaste["cidjrbentou"] = jbovlaste["cidja"];
+            System.Console.WriteLine(jbovlaste["cidja"].Notes);
+            System.Console.WriteLine(jbovlaste["cidjrbentou"].Notes);
         }
     }
 }
