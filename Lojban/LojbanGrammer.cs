51d0e15e9d7f4b3b9faab8003386aa0646710a8b
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-11 17:49:44 +0900

    Add Lojban Test
    Alphabet

---
+++ Lojban/LojbanGrammer.cs
@@ -0,0 +1,11 @@
+﻿namespace Lojban
+{
+    public static class LojbanGrammer
+    {
+        public static string Comma { get; } = @"[,]";
+
+        public static string Space_Char { get; } = @"[.?! ]" + "|" + Space_Char1 + "|" + Space_Char2;
+        public static string Space_Char1 { get; } = @"\t";
+        public static string Space_Char2 { get; } = @"\n";
+    }
+}
--- LojbanTest/Program.cs
+++ LojbanTest/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace LojbanTest
 {
+    public static class LojbanGrammer
+    {
+
+        //--------------------------------------------------------------------------------
+
+        public static NotImplementedException LojbanWord => new NotImplementedException();
+        public static NotImplementedException Nucleus => new NotImplementedException();
+        public static NotImplementedException Affricate => new NotImplementedException();
+
+        //--------------------------------------------------------------------------------
+
+        public static string Syllabic => l + "|" + m + "|" + n + "|" + r;
+        public static string Voiced => b + "|" + d + "|" + g + "|" + j + "|" + v + "|" + z;
+        public static string Unvoiced => c + "|" + f + "|" + k + "|" + p + "|" + s + "|" + t + "|" + x;
+        public static string l => Comma + @"*[lL](?!" + h + ")(?![lL])";
+        public static string m => Comma + @"*[mM](?!" + h + ")(?![mM])(?!" + z + ")";
+        public static string n => Comma + @"*[nN](?!" + h + ")(?![nN])(?!" + Affricate + ")";
+        public static string r => Comma + @"*[rR](?!" + h + ")(?![rR])";
+        public static string b => Comma + @"*[bB](?!" + h + ")(?![bB])(?!" + Unvoiced + ")";
+        public static string d => Comma + @"*[dD](?!" + h + ")(?![dD])(?!" + Unvoiced + ")";
+        public static string g => Comma + @"*[gG](?!" + h + ")(?![gG])(?!" + Unvoiced + ")";
+        public static string v => Comma + @"*[vV](?!" + h + ")(?![vV])(?!" + Unvoiced + ")";
+        public static string j => Comma + @"*[jJ](?!" + h + ")(?![jJ])(?!" + z + ")(?!" + Unvoiced + ")";
+        public static string z => Comma + @"*[zZ](?!" + h + ")(?![zZ])(?!" + j + ")(?!" + Voiced + ")";
+        public static string s => Comma + @"*[sS](?!" + h + ")(?![sS])(?!" + c + ")(?!" + Voiced + ")";
+        public static string c => Comma + @"*[cC](?!" + h + ")(?![cC])(?!" + s + ")(?!" + x + ")(?!" + Voiced + ")";
+        public static string x => Comma + @"*[xX](?!" + h + ")(?![xX])(?!" + c + ")(?!" + k + ")(?!" + Voiced + ")";
+        public static string k => Comma + @"*[kK](?!" + h + ")(?![kK])(?!" + x + ")(?!" + Voiced + ")";
+        public static string f => Comma + @"*[fF](?!" + h + ")(?![fF])(?!" + Voiced + ")";
+        public static string p => Comma + @"*[pP](?!" + h + ")(?![pP])(?!" + Voiced + ")";
+        public static string t => Comma + @"*[tT](?!" + h + ")(?![tT])(?!" + Voiced + ")";
+        public static string h => Comma + @"*['h](?" + Nucleus + ")";
+
+        //--------------------------------------------------------------------------------
+
+        public static string Digit => Comma + @"*%d(?!" + h + ")(?!" + Nucleus + ")";
+        public static string PostWord => Pause + @"|(?!" + Nucleus + ")" + LojbanWord;
+        public static string Pause => Comma + @"*" + SpaceChar + "|" + EOF;
+        public static string EOF => Comma + @"*(?!.)";
+        public static string Comma => @"[,]";
+        public static string NonLojbanWord => @"[,]";
+        public static string NonSpace => @"[,]";
+
+        // Unicode-style and escaped chars not compatible with cl-peg
+        // space-char <- [.\t\n\r?!\u0020]
+        public static string SpaceChar => @"[.?! ]" + "|" + SpaceChar1 + "|" + SpaceChar2;
+        public static string SpaceChar1 => @"\t";
+        public static string SpaceChar2 => @"\n";
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var result = Regex.Match("ki'mo", LojbanGrammer.h);
+            Console.WriteLine("Success : " + result.Success);
+            Console.WriteLine("Value   : " + result.Value);
         }
     }
 }
