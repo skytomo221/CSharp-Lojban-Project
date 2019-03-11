f2be6ba2cfba26a7b31d1c03ce397b078cb48527
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-11 23:28:56 +0900

    Add Sprache

--- LojbanTest/Program.cs
+++ LojbanTest/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Sprache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
-
 
 namespace LojbanTest
 {
@@ -67,13 +69,55 @@ namespace LojbanTest
 
     }
 
+    public class Property
+    {
+        public string Key { get; private set; }
+        public int Value { get; private set; }
+        public Property(string key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public static class ConfigParser
+    {
+        public static readonly Parser<string> Key = Parse.Letter.AtLeastOnce().Text();
+        public static readonly Parser<int> Value = Parse.Decimal.Select(n => Convert.ToInt32(n));
+        public static readonly Parser<Property> Property = from key in Key.Token()
+                                                           from asign in Parse.Char('=').Token()
+                                                           from value in Value.Token()
+                                                           select new Property(key, value);
+        public static readonly Parser<IEnumerable<Property>> Properties = Property.Many().End();
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            var result = Regex.Match("ki'mo", LojbanGrammer.h);
-            Console.WriteLine("Success : " + result.Success);
-            Console.WriteLine("Value   : " + result.Value);
+            //var src = "speed = 300\ntest = 200";
+            //var p = ConfigParser.Properties.Parse(src);
+            //foreach (var item in p)
+            //{
+            //    Console.WriteLine(item.Key + "=" + item.Value);
+            //}
+
+            var input = "(cat))";
+
+            // Sparache
+            Parser<string> parser = from left in Parse.Char('(')
+                                    from main in Parse.CharExcept(new char[] { '(', ')'}).Many().Text()
+                                    from right in Parse.Char(')')
+                                    select main;
+            string result = parser.Parse(input);
+            Console.WriteLine("Sprache: " + result + " (" + result + ")");
+
+            // 正規表現
+            var match = Regex.Match(input, @"(\(*).*(\)*)");
+            result = match.Value;
+            Console.WriteLine("Regex: " + result + " (" + result + ")");
+
         }
     }
 }
