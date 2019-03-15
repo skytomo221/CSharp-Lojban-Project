517401e92ffc9a311735c35ab7868223d10050cb
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-15 22:31:02 +0900

    Fix Pegasus Converter

--- PegasusConverter/Program.cs
+++ PegasusConverter/Program.cs
@@ -5,14 +5,6 @@ using System.Text.RegularExpressions;
 
 namespace PegasusParser
 {
-    struct Expression
-    {
-        public string Left { get; set; }
-        public string Right { get; set; }
-        public Expression(string left, string right) { Left = left; Right = right; }
-        public Expression(string comment) { Left = comment; Right = string.Empty; }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -26,16 +18,21 @@ namespace PegasusParser
             foreach (var sentence in peg.Split('\n'))
             {
                 var matches = Regex.Matches(sentence, @"(?<left>.*) <- (?<right>.*)");
-                //Console.WriteLine(matches.Count);
                 if (matches.Count == 1)
                 {
                     var left = matches[0].Groups["left"].Value.Replace("-", "_");
                     var right = matches[0].Groups["right"].Value.Replace("-", "_");
-                    peggcs += left + " <Node> <- expr: (" + right + ") { new Node(\"" + left + "\", expr) }";
+                    if(Regex.IsMatch(right, "#.*$"))
+                    {
+                        right = Regex.Replace(right, "#(?<comment>.*)$", "/*${comment}*/");
+                    }
+                    peggcs += left + " <Node> = expr: (" + right + ") { new Node(\"" + left + "\", expr) }";
                 }
                 else
                 {
-                    peggcs += sentence;
+                    peggcs += (Regex.IsMatch(sentence, "^#.*"))
+                    ? Regex.Replace(sentence, "^#(?=.*)", "//")
+                    : sentence;
                 }
                 peggcs += "\n";
             }
