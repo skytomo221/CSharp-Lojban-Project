ab23520cf03c56489aceca1b87dfa5f20ccc3bf3
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-16 11:07:00 +0900

    Add Lojban class library 😀

---
+++ Lojban/LojbanParser.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Lojban
+{
+    public class LojbanParser
+    {
+        public string Json { get => JsonConvert.SerializeObject(Result, Formatting.Indented); }
+        public string Text { get; set; }
+        public object Result { get; private set; }
+        public LojbanParser() { }
+        public LojbanParser(string text) { Text = text; }
+        public object Parse()
+        {
+            var parser = new Camxes();
+            return Result = parser.Parse(Text);
+        }
+        public object Parse(string text)
+        {
+            var parser = new Camxes();
+            return Result = parser.Parse(Text = text);
+        }
+    }
+}
