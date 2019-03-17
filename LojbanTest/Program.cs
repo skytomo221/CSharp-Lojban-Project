5c0812c944b026c303a4db731b85e1293442c22c
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-17 19:01:39 +0900

    Tweak Lojban Test

--- LojbanTest/Program.cs
+++ LojbanTest/Program.cs
@@ -9,9 +9,15 @@ namespace LojbanTest
         static void Main(string[] args)
         {
             var text = "la.alis.co'a tatpi lo nu zutse lo rirxe korbi re'o lo mensi gi'e zukte fi no da";
+            text = "coi";
             var parser = new LojbanParser();
+            parser.ParserFile = "camxes";
             var result = parser.Parse(text);
+            Console.WriteLine(result);
+            parser.ParserFile = "camxes-beta";
+            result = parser.Parse(text);
             Console.WriteLine(result);
         }
     }
 }
+
