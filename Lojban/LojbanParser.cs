321255d0d3a4fa12b26cd244718eb5a6ae5819de
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-19 17:43:47 +0900

    Add Parser Mode

--- Lojban/LojbanParser.cs
+++ Lojban/LojbanParser.cs
@@ -5,18 +5,6 @@ using System.Windows.Forms;
 
 namespace Lojban
 {
-    [Flags]
-    public enum ParseMode
-    {
-        Indented = 0b0000001,
-        KeepMorphology = 0b0010000,
-        ShowSpaces = 0b0001000,
-        ShowTerminators = 0b0000010,
-        ShowWordClasses = 0b0000100,
-        RawOutput = 0b0100000,
-        ShowMainNodeLabels = 0b1000000,
-    }
-
     public class LojbanParser
     {
         public string Text { get; set; } = "coi";
---
+++ Lojban/ParseMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lojban
+{
+    [Flags]
+    public enum ParseMode
+    {
+        Indented = 0b0000001,
+        KeepMorphology = 0b0010000,
+        ShowSpaces = 0b0001000,
+        ShowTerminators = 0b0000010,
+        ShowWordClasses = 0b0000100,
+        RawOutput = 0b0100000,
+        ShowMainNodeLabels = 0b1000000,
+    }
+}
